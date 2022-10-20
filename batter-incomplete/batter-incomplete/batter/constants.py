a62f434e305d8831334d33a78bcc6c75a8ab0d51
AGB004 <alecbyington@gmail.com>
2022-10-19 18:00:09 -0600

    update batter/

--- batter-incomplete/batter-incomplete/batter/constants.py
+++ batter-incomplete/batter-incomplete/batter/constants.py
@@ -55,7 +55,7 @@ IN_PLAY = 3
 GAME_OVER = 4
 
 # LEVELS
-LEVEL_FILE = "assets/data/level-{:03}.png"
+LEVEL_FILE = "batter/assets/data/level-{:03}.txt"
 BASE_LEVELS = 5
 
 # -------------------------------------------------------------------------------------------------- 
